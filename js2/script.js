0f28d982c9d1766fecef580d9271db1f4cbf221d
Burak Yüksel <burak.yueksel@hotmail.com>
2023-11-06 11:09:37 +0100

    added calculator functionality

---
+++ js2/script.js
@@ -0,0 +1,14 @@
+let meal = 22;
+let tip = 0.1;
+
+let tipAmount = meal * tip;
+let mealCost = meal + tipAmount;
+
+console.log(
+  "The meal is: " +
+    meal +
+    " and the tip is " +
+    tipAmount +
+    " and the total cost is " +
+    mealCost
+);
