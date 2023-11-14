68c6aba3081ce8e9e538cfc729519d81eface931
Burak Yüksel <burak.yueksel@hotmail.com>
2023-11-14 16:14:59 +0100

    added exercise Star

--- js-loops_stars/js/index.js
+++ js-loops_stars/js/index.js
@@ -2,13 +2,24 @@ console.clear();
 
 const starContainer = document.querySelector('[data-js="star-container"]');
 
-function renderStars() {
+function renderStars(filledStars) {
   // reset the star container before rerendering stars
   starContainer.innerHTML = "";
 
   //--v-- your code here --v--
-
+  for (let i = 1; i <= 5; i++) {
+    if (filledStars >= 1) {
+      const img = document.createElement("img");
+      img.src = "assets/star-filled.svg";
+      starContainer.append(img);
+      filledStars--;
+    } else {
+      const img = document.createElement("img");
+      img.src = "assets/star-empty.svg";
+      starContainer.append(img);
+    }
+  }
   //--^-- your code here --^--
 }
 
-renderStars();
+renderStars(5);
