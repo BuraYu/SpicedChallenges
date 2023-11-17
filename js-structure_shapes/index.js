5123d39d442252048f19bade623934b48ad3a65a
Burak Yüksel <burak.yueksel@hotmail.com>
2023-11-17 11:38:24 +0100

    added exercise 1

--- js-structure_shapes/index.js
+++ js-structure_shapes/index.js
@@ -1,23 +1,14 @@
 console.clear();
 
+import { getRandomColor } from "./utils/randomColor.js";
+import Circle from "./components/Circle/circle.js";
+import Pentagon from "./components/Pentagon/pentagon.js";
+import Square from "./components/Square/square.js";
+
 const root = document.getElementById("root");
 
-const circle = document.createElement("div");
-circle.classList.add("circle");
-circle.addEventListener("click", () => {
-  circle.style.backgroundColor = "#ccc";
-});
+let circleElement = Circle();
+let squareElement = Square();
+let pentagonElement = Pentagon();
 
-const square = document.createElement("div");
-square.classList.add("square");
-square.addEventListener("click", () => {
-  square.style.backgroundColor = "#ccc";
-});
-
-const pentagon = document.createElement("div");
-pentagon.classList.add("pentagon");
-pentagon.addEventListener("click", () => {
-  pentagon.style.backgroundColor = "#ccc";
-});
-
-root.append(circle, square, pentagon);
+root.append(circleElement, squareElement, pentagonElement);
