73f01306ad7d929d112ef9107f7078ce996dd2ac
Burak Yüksel <burak.yueksel@hotmail.com>
2023-11-27 11:17:41 +0100

    add 3 react exercises

---
+++ react-basics_hello-world-article/src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <>
+      <article>
+        <h1>Hello World</h1>
+        <p>
+          This is a article about the world of programming Lorem ipsum dolor,
+          sit amet consectetur adipisicing elit. Molestiae, eius minima eligendi
+          veritatis suscipit repudiandae quia qui placeat velit, unde est
+          corrupti alias eveniet rerum totam quod odit. Doloribus, ipsa.
+        </p>
+      </article>
+      ;
+    </>
+  );
+}
