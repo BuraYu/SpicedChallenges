364344479d371c40cc21720725c59b135a2f828c
Burak Yüksel <burak.yueksel@hotmail.com>
2023-11-27 10:49:36 +0100

    added challenges

--- js-async-functions_slot-machine/index.js
+++ js-async-functions_slot-machine/index.js
@@ -2,6 +2,7 @@ import { Wheel } from "./components/Wheel/Wheel.js";
 import { SpinButton } from "./components/SpinButton/SpinButton.js";
 import { Machine } from "./components/Machine/Machine.js";
 import { Result } from "./components/Result/Result.js";
+import { getMaxCount } from "./utils/symbols.js";
 
 console.clear();
 
@@ -22,6 +23,7 @@ root.append(machine, spinButton, result);
 //                                      ↙️
 spinButton.addEventListener("click", async () => {
   spinButton.disabled = true;
+
   /**
    * Hint 1:
    * The wheel elements have a spin method that returns a promise.
