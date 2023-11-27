65e96485d712932e141217790544ecaf4cbe5013
Burak Yüksel <burak.yueksel@hotmail.com>
2023-11-27 16:09:01 +0100

    add solution for exercises

--- react-props_button/src/App.js
+++ react-props_button/src/App.js
@@ -2,5 +2,36 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  function handleClick() {
+    console.log("Click");
+  }
+
+  return (
+    <Button
+      color="blue"
+      disable={false}
+      text="knopf lol"
+      onClick={handleClick}
+    />
+  );
+}
+
+function Button(props) {
+  if (props.disable) {
+    return (
+      <button color={props.color} disabled={props.disable}>
+        {props.text}
+      </button>
+    );
+  } else {
+    return (
+      <button
+        color={props.color}
+        disabled={props.disable}
+        onClick={props.onClick}
+      >
+        {props.text}
+      </button>
+    );
+  }
 }
--- react-props_smiley/src/App.js
+++ react-props_smiley/src/App.js
@@ -2,5 +2,9 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  return <Smiley isHappy={true} />;
+}
+
+function Smiley(props) {
+  return <h1>{props.isHappy ? ":)" : ":("}</h1>;
 }
