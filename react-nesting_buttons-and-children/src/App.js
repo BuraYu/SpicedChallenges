cf48a4c44898e9328d127cf1c862dc8766d63702
Burak Yüksel <burak.yueksel@hotmail.com>
2023-11-29 10:41:53 +0100

    solution for exercise children

--- react-nesting_buttons-and-children/src/App.js
+++ react-nesting_buttons-and-children/src/App.js
@@ -4,18 +4,18 @@ import "./styles.css";
 export default function App() {
   return (
     <main>
-      <Button />
-      <Button />
-      <Button />
-      <Button />
+      <Button>Knopf</Button>
+      <Button>Button</Button>
+      <Button>Dügme</Button>
+      <Button>단추</Button>
     </main>
   );
 }
 
-function Button() {
+function Button({ children }) {
   return (
     <button className="button" type="button">
-      Click me!
+      {children}
     </button>
   );
 }
---
+++ react-nesting_navigation/src/components/Logo.js
@@ -0,0 +1,6 @@
+import logo from "../img/logo.jpg";
+import { Image } from "./Image.js";
+
+export const Logo = () => {
+  return <Image src={logo} alt="logo" />;
+};
